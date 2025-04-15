cfe2789a155e9ad2364262c566900112a24848bd
Hayssa Costa <hayssa.hscosta@gmail.com>
2025-04-14 23:00:30 -0400

    Just some gibberish

--- js-projects/tic-tac-toe/scripts.js
+++ js-projects/tic-tac-toe/scripts.js
@@ -9,4 +9,44 @@
 // 3.2. if there is no winner, changePlayer() 
 // 4. Apply conditions to win:
 // 4.1. to win, we must have the following places with the same symbol, whatever they are:
-// [0, 1, 2], [0, 4, 8], [0, 3, 6], [3, 4, 5], [6, 7, 8], [2, 5, 8], [1, 4, 7], [2, 4, 6]
+
+const board = ["", "", "", "", "", "", "", ""]
+
+function createPlayer(name, symbol) {
+    this.name = name;
+    this.symbol = symbol;
+    this.getSymbol = () => symbol;
+    this.getName = () => name;
+}
+
+function ticTacToe() {
+    this.board = board;
+    const getBoard = () => board;
+    const playOnBoard = (position, player) => {
+        board[position] = player.getSymbol();
+    }
+    const verifyResults = (wantedSymbol, testPosition) => {
+        if (testHorizontalWinner(0) || testHorizontalWinner(3) || testHorizontalWinner(6)) {
+            return true;
+        } else if (testVerticalWinner(0) || testVerticalWinner(1) || testVerticalWinner(3)) {
+            return true;
+        } else if (testCrossWinner(0)) {
+            return true
+        } else if (testCrossInverseWinner(2)) {
+            return true;
+        }
+        return false
+    }
+    const testHorizontalWinner =  (position) => {
+        return board[position + 1] == board[position + 2]
+    }
+    const testCrossInverseWinner = (position) => {
+        return board[position + 2] == board[position + 4]
+    }
+    const testVerticalWinner = (position) => {
+        return board[position + 3] == board[position + 6]
+    }
+    const testCrossWinner = (position) => {
+        return board[position + 4] == board[position + 8]
+    }
+}
